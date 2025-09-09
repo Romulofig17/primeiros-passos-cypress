8b11546d0ccc734515081e75097914991e8662fa
Romulofig <afromuloabreu@gmail.com>
2025-09-08 21:14:16 -0300

    Readme.md added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -3,14 +3,19 @@ describe("Orange HRM Tests", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
-    cy.get(
-      ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input"
-    ).type("Admin");
-    cy.get(
-      ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input"
-    ).type("admin123");
-    cy.get(".oxd-button").click();
+    cy.get("[name='username']").type("Admin");
+    cy.get("[name='password']").type("admin123");
+    cy.get("[type='submit']").click();
     cy.location("pathname").should("equal", "/web/index.php/dashboard/index");
     cy.get(".oxd-topbar-header-breadcrumb > .oxd-text");
+  });
+  it("Login - Fail", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get("[name='username']").type("Test");
+    cy.get("[name='password']").type("test");
+    cy.get("[type='submit']").click();
+    cy.get("[role='alert']");
   });
 });
