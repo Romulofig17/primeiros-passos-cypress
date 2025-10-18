0f87742814494e3101e2f57f71211f0f3acae97d
Romulofig <afromuloabreu@gmail.com>
2025-10-18 01:05:52 -0300

    Add page objects

---
+++ cypress/pages/menuPage.js
@@ -0,0 +1,15 @@
+class menuPage {
+  selectorsList() {
+    const selectors = {
+      myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    };
+
+    return selectors;
+  }
+
+  accessMyInfo() {
+    cy.get(this.selectorsList().myInfoButton).click();
+  }
+}
+
+export default menuPage;
